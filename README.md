d47489033ebd82f5333842a85be79ca7956e2937
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-11-23 20:38:56 +0100

    :rocket:

---
+++ README.md
@@ -0,0 +1,33 @@
+# bare-app-image
+
+AppImage packaging tools for Bare.
+
+```
+npm i bare-app-image
+```
+
+## Usage
+
+```js
+const createAppImage = require('bare-app-image')
+
+await createAppImage('./path/to/my.AppDir', './app.AppImage')
+```
+
+## API
+
+#### `await createAppImage(source[, destination][, options])`
+
+Options include:
+
+```js
+options = {
+  compression,
+  sign,
+  key
+}
+```
+
+## License
+
+Apache-2.0
---
+++ index.js
@@ -0,0 +1,27 @@
+const run = require('./lib/run')
+const appimagetool = require('./lib/appimagetool')
+
+module.exports = async function createAppImage(source, destination, opts = {}) {
+  if (typeof destination === 'object' && destination !== null) {
+    opts = destination
+    destination = null
+  }
+
+  const { compression = null, sign = false, key = null } = opts
+
+  const args = []
+
+  if (compression) args.push('--comp', compression)
+
+  if (sign) {
+    args.push('--sign')
+
+    if (key) args.push('--sign-key', key)
+  }
+
+  args.push(source)
+
+  if (destination) args.push(destination)
+
+  await run(appimagetool, args)
+}
